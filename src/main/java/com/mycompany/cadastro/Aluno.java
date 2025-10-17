d1a700692aea075c2fffcd726828520df1504c53
allysson-assuncao <anybody723@proton.me>
2025-10-17 20:31:25 -0300

    cadastro de alunos

--- src/main/java/com/mycompany/cadastro/Aluno.java
+++ src/main/java/com/mycompany/cadastro/Aluno.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.cadastro;
 
-/**
- *
- * @author janio.silva
- */
 public class Aluno {
-    private String nome; private String cpf; private String endereco;
-    private String email;
+    private String nome, cpf, endereco, email, cep, cidade, estado;
 
     public String getNome() {
         return nome;
@@ -43,6 +34,31 @@ public class Aluno {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
     
 }
--- src/main/java/com/mycompany/cadastro/Cadastro.java
+++ src/main/java/com/mycompany/cadastro/Cadastro.java
@@ -1,24 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.cadastro;
 
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 import java.io.Serializable;
 
-/**
- *
- * @author janio.silva
- */
 @Named
 @SessionScoped
 public class Cadastro implements Serializable  {
     private String mensagem="Bem vindo ao cadastro de Alunos!";
-    private Aluno dadosAluno;
+    private Aluno dadosAluno = new Aluno();
     
     public void cadastrarAluno(){
+        ServicosAluno s = new Ser
         this.mensagem="Aluno cadastrado com sucesso!";
     }   
 
